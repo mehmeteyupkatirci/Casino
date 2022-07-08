cfafa98aa1293a8fea3a530563e522347af4e53a
mehmeteyupkatirci <mehmeteyupkatirci@gmail.com>
2022-07-08 13:31:16 +0300

    first page nums added

--- Casino/MainWindow.xaml.cs
+++ Casino/MainWindow.xaml.cs
@@ -42,5 +42,29 @@ namespace Casino
         {
             Environment.Exit(0);
         }
+
+        private void Spin_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int count = 0;
+            var coin = Convert.ToInt32(txtCoin.Text);
+            for (int i = 0; i < 2000; i++)
+            {
+                var master = rnd.Next(0, 36);
+
+                if (master == 24)
+                {
+                    MessageBox.Show(" 24 GELDİ KAZANDINIZ \n TOPLAM ÇEVİRME : "+ count + "\n SONUÇ: " + master.ToString());
+                    coin += 36;
+                    i = 2000;
+                }
+                else
+                {
+                    count++;
+                    coin--;
+                }
+            }
+            txtCoin.Text = coin.ToString();
+        }
     }
 }
